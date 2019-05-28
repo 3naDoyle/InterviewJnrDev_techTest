4ecc0e4406ee719a615efdeea14fec83425523b9
Triona Doyle <tdoyle@internal.onaware.com>
2019-05-28 17:37:58 +0100

    changes

--- src/com/triona/solution/main/Main.java
+++ src/com/triona/solution/main/Main.java
@@ -8,7 +8,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.function.Predicate;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 import com.triona.company.Worker;
